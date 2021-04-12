923fd0447fdd0c9fd2c4090220b88025ca2a93a2
Nelson Foltz <nelsfoltz@gmail.com>
2021-04-12 17:21:11 -0400

    add: 46: banner component basics

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Hero from '../globals/Hero';
-import aboutImg from '../../images/aboutBcg.jpeg';
+// import aboutImg from '../../images/aboutBcg.jpeg';
 import homeImg from '../../images/homeBcg.jpeg';
+import Banner from '../globals/Banner';
 
 const Header = () => {
   return (
     <Hero img={homeImg}>
-      <h1>hello world</h1>
+      <Banner
+        greeting="welcome to"
+        title="beachwalk resort"
+        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui architecto cumque quibusdam ducimus sit maxime."
+      />
     </Hero>
   );
 };
