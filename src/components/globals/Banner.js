ea383ea1b7eec251f569ab281ece36bf58d4daeb
Nelson Foltz <nelsfoltz@gmail.com>
2021-04-17 09:45:48 -0400

    add: 48: Banner component helper functions

--- src/components/globals/Banner.js
+++ src/components/globals/Banner.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled, { css, keyframes } from 'styled-components';
-import { setColor } from '../../styles';
+import {
+  setColor,
+  setRem,
+  setLetterSpacing,
+  setBorder,
+  media,
+} from '../../styles';
 
 const Banner = ({ className, title, text, children, greeting }) => {
   return (
@@ -19,15 +25,13 @@ const Banner = ({ className, title, text, children, greeting }) => {
 const BannerWrapper = styled(Banner)`
   background: rgba(0, 0, 0, 0.7);
   text-align: center;
-  padding: 60px 32px;
-  /* setRem */
-  letter-spacing: 3px;
-  /* setLetterSpacing */
+  padding: ${setRem(60)} ${setRem(32)};
+  ${setLetterSpacing(3)};
+
   color: ${setColor.mainWhite};
   h1 {
     text-transform: capitalize;
-    font-size: 48px;
-    /* setRem */
+    font-size: ${setRem(48)};
     color: ${setColor.primaryColor};
     span {
       color: ${setColor.mainWhite};
@@ -37,13 +41,12 @@ const BannerWrapper = styled(Banner)`
     width: 85%;
     margin: 0 auto;
   }
-  @media (min-width: 768px) {
-    width: 70vw;
-    border: 6px solid ${setColor.primaryColor};
+  ${media.tablet` width: 70vw;
+    ${setBorder({ width: '6px', color: setColor.primaryColor })};
     p {
       width: 75%;
-    }
-  }
+    }`}
+
   h1 {
     /* animation */
   }
